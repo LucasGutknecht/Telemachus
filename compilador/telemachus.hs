3bf0772c2885d9b8e29047be73210bd374698153
Lucas <lucas@albesbrasil.com.br>
2025-08-30 11:15:54 -0300

    Melhoria Feuta: Adicionar remoção dos arquivos *.i e *.s

--- compilador/telemachus.hs
+++ compilador/telemachus.hs
@@ -2,16 +2,15 @@ import System.Environment
 import System.Process
 import System.FilePath (dropExtension, takeExtension)
 import System.Exit (ExitCode(ExitSuccess, ExitFailure))
-import System.Directory (doesFileExist)
+import System.Directory (doesFileExist, removeFile)
 import Control.Monad (when)
 
 -- Melhorias a fazer: 1 - Corrigir a injeção de shell
--- Melhorias a fazer: 2 - Adicionar remoção dos arquivos *.i e *.s
--- Melhorias a fazer: 3 - Melhorar o tratamento de erros
--- Melhorias a fazer: 4 - Melhorar as mensagens de erros
--- Melhorias a fazer: 5 - Adicionar mais opcoes de flags do compilador
--- Melhorias a fazer: 6 - Adicionar a opcao do Lexer para produzir a lista tokens
--- Melhorias a fazer: 7 - Adicionar o parser
+-- Melhorias a fazer: 2 - Melhorar o tratamento de erros
+-- Melhorias a fazer: 3 - Melhorar as mensagens de erros
+-- Melhorias a fazer: 4 - Adicionar mais opcoes de flags do compilador
+-- Melhorias a fazer: 5 - Adicionar a opcao do Lexer para produzir a lista tokens
+-- Melhorias a fazer: 6 - Adicionar o parser
 
 contemExtensaoC :: String -> Bool
 contemExtensaoC s = takeExtension s == ".c"
@@ -61,6 +60,26 @@ compilarArquivoExecutavel arquivo = do
         ExitSuccess -> return Nothing
         ExitFailure _ -> return (Just "Erro na geração do executável.")
 
+removerArquivosIntermediarios :: String -> IO ()
+removerArquivosIntermediarios arquivo = do
+    let arquivos = [dropExtension arquivo ++ ext | ext <- [".i", ".s"]]
+    mapM_ removeFileIfExists arquivos
+
+removeFileIfExists :: FilePath -> IO ()
+removeFileIfExists arquivo = do
+    putStrLn $ "Tentando remover: " ++ arquivo
+    existe <- doesFileExist arquivo
+    if existe
+        then do
+            putStrLn $ "Arquivo existe, removendo: " ++ arquivo
+            removeFile arquivo
+            putStrLn $ "Arquivo removido: " ++ arquivo
+        else
+            putStrLn $ "Arquivo não encontrado: " ++ arquivo
+
+removeFiles :: [FilePath] -> IO ()
+removeFiles = mapM_ removeFileIfExists
+
 executarEtapa :: IO (Maybe String) -> String -> IO Bool
 executarEtapa acao mensagemSucesso = do
     resultado <- acao
@@ -94,4 +113,6 @@ main = do
                             _ <- executarEtapa
                                 (compilarArquivoExecutavel x)
                                 "Executável gerado com sucesso!"
+                            removeFiles [dropExtension x ++ ".i", dropExtension x ++ ".s"]
+                            putStrLn "Arquivos intermediários removidos com sucesso."
                             return ()
